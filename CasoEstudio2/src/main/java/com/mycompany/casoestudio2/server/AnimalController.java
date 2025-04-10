85bca14b5b9bd9e04895aaccaa7299e9259e3a96
lsalasz07 <luisignaciosalassalazar19@gmail.com>
2025-04-09 20:43:36 -0600

    Add files via upload

---
+++ CasoEstudio2/src/main/java/com/mycompany/casoestudio2/server/AnimalController.java
@@ -0,0 +1,15 @@
+package com.mycompany.casoestudio2.server;
+
+import com.mycompany.casoestudio2.farm.Animal;
+import java.util.List;
+
+public class AnimalController {
+
+    List<Animal> obtenerAnimal() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    boolean insertarAnimal(Animal animal) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+}
--- CasoEstudio2/src/main/java/com/mycompany/casoestudio2/server/ClientHandler.java
+++ CasoEstudio2/src/main/java/com/mycompany/casoestudio2/server/ClientHandler.java
@@ -5,6 +5,34 @@ import java.io.*;
 import java.net.Socket;
 import java.util.List;
 
-public class ClientHandler {
+public class ClientHandler extends Thread {
+    private Socket clienteSocket;
+    private AnimalController animalController = new AnimalController();
     
+    public ClientHandler (Socket socket) {
+        this.clienteSocket = socket;
+    }
+    
+    @Override
+    public void run () {
+        try (PrintWriter out = new PrintWriter(clienteSocket.getOutputStream(), true);
+             BufferedReader in = new BufferedReader(new InputStreamReader(clienteSocket.getInputStream()))) {
+            
+            String inputLine;
+            while ((inputLine = in.readLine ()) != null) {
+                 System.out.println("La peticion del cliente es: " + inputLine);
+                 String response = processRequest(inputLine);
+                 out.println(response);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                clienteSocket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+  }
 }
+    
